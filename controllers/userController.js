370e0a880e0c376a74ebf76590e8101212a101c4
Ingrid <mtra.ingrid.lopez@gmail.com>
2025-01-17 12:30:49 -0800

    Se inició el proyecto.

---
+++ controllers/userController.js
@@ -0,0 +1,18 @@
+const userService = require('../services/userService');
+
+const getAllUsers = async (req, res) => {
+  try {
+    const users = await userService.getUsers();
+    res.status(200).json(users);
+  } catch (error) {
+    res.status(500).json({ error: 'Error fetching users' });
+  }
+};
+
+const testing = (req, res) => {
+  res.status(200).json({ name:"Hola Cirus" });
+}
+
+module.exports = {
+  getAllUsers,
+};
---
+++ services/userService.js
@@ -0,0 +1,10 @@
+const db = require('../config/db');
+
+const getUsers = async () => {
+  const [rows] = await db.query('SELECT * FROM users');
+  return rows;
+};
+
+module.exports = {
+  getUsers,
+};
